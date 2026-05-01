b8ae3b2eacfcd87e9a8c1ecca9d6f44374e4cb25
keshavi <kesavik@uebiz.com>
2026-05-01 19:58:39 +0530

    added terraform files

---
+++ modules/vnet/variables.tf
@@ -0,0 +1,28 @@
+variable "name" {
+  description = "VNet name"
+  type        = string
+}
+
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "address_space" {
+  type = list(string)
+}
+
+variable "subnets" {
+  description = "Map of subnet configurations"
+  type = map(object({
+    address_prefixes = list(string)
+  }))
+}
+
+variable "tags" {
+  type    = map(string)
+  default = {}
+}
