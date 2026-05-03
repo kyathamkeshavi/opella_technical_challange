70187a473179d9c8b509c7a20c0605313469012f
keshavi <kesavik@uebiz.com>
2026-05-03 19:33:20 +0530

    trigger pripeline

--- modules/keyvault/outputs.tf
+++ modules/keyvault/outputs.tf
@@ -1,7 +1,7 @@
-output "vnet_id" {
-  value = azurerm_virtual_network.azvnet.id
-}
+# output "vnet_id" {
+#   value = azurerm_virtual_network.azvnet.id
+# }
 
-output "subnet_ids" {
-  value = { for k, v in azurerm_subnet.azsubnet : k => v.id }
-}
+# output "subnet_ids" {
+#   value = { for k, v in azurerm_subnet.azsubnet : k => v.id }
+# }
